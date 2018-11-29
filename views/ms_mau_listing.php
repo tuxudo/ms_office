210e4c3dc297cea0dea500684c3560ad1a1cf695
tuxudo <tuxudo@users.noreply.github.com>
2018-11-29 13:13:52 -0500

    Fixed whitespace

--- views/ms_mau_listing.php
+++ views/ms_mau_listing.php
@@ -11,7 +11,6 @@ new Ms_office_model;
   <div class="row">
   	<div class="col-lg-12">
 	<h3><span data-i18n="ms_office.mau_report"></span> <span id="total-count" class='label label-primary'>…</span></h3>
-
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
 		      <tr>
@@ -32,8 +31,8 @@ new Ms_office_model;
 		    </thead>
 		    <tbody>
 		    	<tr>
-					<td data-i18n="listing.loading" colspan="13" class="dataTables_empty"></td>
-				</tr>
+		    	     <td data-i18n="listing.loading" colspan="13" class="dataTables_empty"></td>
+		    	</tr>
 		    </tbody>
 		  </table>
     </div> <!-- /span 13 -->
@@ -47,7 +46,6 @@ new Ms_office_model;
 		var oTable = $('.table').DataTable();
 		oTable.ajax.reload();
 		return;
-
 	});
 
 	$(document).on('appReady', function(e, lang) {
@@ -94,44 +92,51 @@ new Ms_office_model;
 	        	var sn=$('td:eq(1)', nRow).html();
 	        	var link = mr.getClientDetailLink(name, sn, '#tab_ms_office-tab');
 	        	$('td:eq(0)', nRow).html(link);
+			    
+	        	// Format time, if timestamp
+	        	var updatecheck = $('td:eq(2)', nRow).html(); 
+	        	if (! isNaN(updatecheck) && updatecheck !== ""){
+	        	     parseInt(updatecheck);
+	        	     var date = new Date(updatecheck * 1000);
+	        	     $('td:eq(2)', nRow).html('<span title="'+moment(date).format('llll')+'">'+moment(date).fromNow()+'</span>');
+	        	}
 
 	        	// Format update button
 	        	var status=$('td:eq(4)', nRow).html();
-                status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
-                (status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
-                $('td:eq(4)', nRow).html(status)
+	        	status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
+	        	(status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
+	        	$('td:eq(4)', nRow).html(status)
                 
 	        	// Format insider box
 	        	var status=$('td:eq(5)', nRow).html();
-                status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
-                (status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
-                $('td:eq(5)', nRow).html(status)
+	        	status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
+	        	(status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
+	        	$('td:eq(5)', nRow).html(status)
                 
 	        	// Format helper tool
 	        	var status=$('td:eq(10)', nRow).html();
-                status = status == 1 ? '<span class="label label-success">'+i18n.t('ms_office.installed')+'</span>' :
-                (status == 0 && status != '' ? '<span class="label label-danger">'+i18n.t('ms_office.not_installed')+'</span>' : '')
-                $('td:eq(10)', nRow).html(status)
+	        	status = status == 1 ? '<span class="label label-success">'+i18n.t('ms_office.installed')+'</span>' :
+	        	(status == 0 && status != '' ? '<span class="label label-danger">'+i18n.t('ms_office.not_installed')+'</span>' : '')
+	        	$('td:eq(10)', nRow).html(status)
                 
 	        	// Format telemetry
 	        	var status=$('td:eq(11)', nRow).html();
-                status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
-                (status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
-                $('td:eq(11)', nRow).html(status)
+	        	status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
+	        	(status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
+	        	$('td:eq(11)', nRow).html(status)
                 
 	        	// Format daemon start
 	        	var status=$('td:eq(12)', nRow).html();
-                status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
-                (status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
-                $('td:eq(12)', nRow).html(status)
-                
-            }
+	        	status = status == 1 ? '<span class="label label-danger">'+i18n.t('enabled')+'</span>' :
+	        	(status == 0 && status != '' ? '<span class="label label-success">'+i18n.t('disabled')+'</span>' : '')
+	        	$('td:eq(12)', nRow).html(status)
+	        }
 	    } );
         
 	    // Use hash as searchquery
 	    if(window.location.hash.substring(1))
 	    {
-			oTable.fnFilter( decodeURIComponent(window.location.hash.substring(1)) );
+		oTable.fnFilter( decodeURIComponent(window.location.hash.substring(1)) );
 	    }
 
 	} );
