731be8341599fef228c1bb827a28d210d3672ef8
tuxudo <tuxudo@users.noreply.github.com>
2018-11-29 12:54:32 -0500

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove ms_office.py script
-rm -f "${MUNKIPATH}preflight.d/ms_office.py"
-
-# Remove ms_office.plist cache file
-rm -f "${MUNKIPATH}preflight.d/cache/ms_office.plist"
