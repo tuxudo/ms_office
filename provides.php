9280bcaec49bd5e173e24affbc2fb650c6b8f593
Jon Crain <joncrain@users.noreply.github.com>
2018-12-10 09:41:37 -0500

    Adding Version Widget (#3)
    * adding MAU update channel widget
    
    * swtiched to general generation check
    
    * generalizing the rest of the generation widgets
    
    * adding Version widget
    
    * fixing links

--- provides.php
+++ provides.php
@@ -15,6 +15,7 @@ return array(
         'ms_outlook_generation' => array('view' => 'ms_outlook_generation_widget', 'i18n' => 'ms_office.outlook_generation'),
         'ms_onenote_generation' => array('view' => 'ms_onenote_generation_widget', 'i18n' => 'ms_office.onenote_generation'),
         'ms_mau_channel' => array('view' => 'ms_mau_channel_widget', 'i18n' => 'ms_office.mau_channel_widget.title'),
+        'ms_version' => array('view' => 'ms_version_widget', 'i18n' => 'ms_office.version.title'),
     ),
     'reports' => array(
         'ms_office_report' => array('view' => 'ms_office_report', 'i18n' => 'ms_office.report'),
